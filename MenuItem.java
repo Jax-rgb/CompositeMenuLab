793a4b6dcea9c5640c3151648179cc4374874b28
Jax-rgb <jacquescarigma@gmail.com>
2026-04-21 10:18:35 +0800

    Create MenuItem.java

---
+++ MenuItem.java
@@ -0,0 +1,20 @@
+// MenuItem.java
+public class MenuItem implements MenuComponent {
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(" - " + name + ": ₱" + String.format("%.2f", price));
+    }
+}
